4faef76da818f8f0b33c454f76c930057a28238e
h3rb <hgilliland@gmail.com>
2016-02-11 14:29:32 -0500

    Added support for Join

--- core/PDO/database.php
+++ core/PDO/database.php
@@ -213,7 +213,7 @@ class Database extends PDO {
    if ( $offset > 0 ) $ending.=' OFFSET '.$offset;
   }
   $query='SELECT '.$columns.' FROM '.$tableA.' '.$type.' JOIN '.$tableB.' '.$on_or_using.' '.$value.$ending.';';
-  $result=$this->db->Execute($query);
+  $result=$this->Execute($query);
   if ( false_or_null($result) ) return array();
   if ( count($result) == 1 ) return array_shift($result);
   return $result;

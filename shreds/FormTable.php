4ce3fcf88bfc90693f9d40bf5019b40364c7fb2f
h3rb <hgilliland@gmail.com>
2016-04-10 23:13:57 -0400

    fixing getData()

--- shreds/FormTable.php
+++ shreds/FormTable.php
@@ -384,6 +384,12 @@ class FormTable extends Unique {
       )
    )."
    }
+   function get_values_$u(o) {
+    var x=row_fields_$u.length;
+    var y=row_ids_$u.length;
+    var o=[];
+    for ( var i=0; i<x; i++ ) for ( var j=0; j<y; j++ ) o[i,j]=$('#_'+i+'_'+j).val();
+   }
    function rem_$u(n,a) {
     var na=Array(); var i,c=0;
     for ( i=0; i<n; i++ ) { na[c]=a[i]; c++; } i++;

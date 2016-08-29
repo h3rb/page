f69ad3e9c287dbf6befad62390a84d8610fb435d
h3rb <hgilliland@gmail.com>
2016-08-29 00:28:22 -0400

    skeleton

---
+++ shreds/Comments.php
@@ -0,0 +1,12 @@
+<?php
+
+ function EnableCommenting(&$p) {
+  $p->CSS('jquery-comments.css');
+  $p->JS('jquery-comments.js');
+ }
+
+
+ function Comments(&$p,$table,$id,$field=NULL) {
+  global $auth;
+
+ }
